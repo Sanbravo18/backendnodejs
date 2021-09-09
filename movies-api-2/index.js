2c034fdc4e79ec2b99e2e91e782e4721975a6049
Daniel Bravo <daniel.brv@hotmail.cl>
2021-09-09 13:21:00 -0300

    Se agrega todo hasta la clase Implementando un CRUD en Express.js

--- movies-api-2/index.js
+++ movies-api-2/index.js
@@ -2,15 +2,10 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
 
 // Cuando hagamos un request a nuestra aplicación, nos imprima un hello world
-app.get('/', (req, res) => {
-    res.send("Hello world");
-  })
-
-app.get('/json', (req, res) => {
-    res.json({hello: 'world'});
-  })
+moviesApi(app);
 
 app.listen(config.port, function () {
     console.log(`Listening http://localhost:${config.port}`);
