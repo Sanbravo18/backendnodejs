fa83fb18449fadc93f4ff6a7888edc1808ee7f94
Daniel Bravo <daniel.brv@hotmail.cl>
2021-09-09 22:15:28 -0300

    Se agrega todo el codigo de mongoDB funcional y operativo 1313

--- movies-api-2/index.js
+++ movies-api-2/index.js
@@ -4,7 +4,11 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
+//body parser
+app.use(express.json());
+
 // Cuando hagamos un request a nuestra aplicación, nos imprima un hello world
+
 moviesApi(app);
 
 app.listen(config.port, function () {
